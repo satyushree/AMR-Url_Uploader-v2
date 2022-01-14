4aaf5262d2b743b5076fe383110bc37d0fbeb61e
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-14 23:04:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,19 +19,18 @@ from pyrogram.errors import ChatAdminRequired
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
+    if not await bot.db.is_user_exist(update.chat.id):
+        await bot.db.add_user(update.chat.id)
+        await bot.send_message(
+            Config.LOG_CHANNEL,
+            f"New User [{update.from_user.first_name}](tg://user?id={update.chat.id}) started."
+        )
+        
     await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
     )
-    if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(Config.LOG_CHANNEL, script.Config.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
-            await db.add_chat(message.chat.id, message.chat.title)
-        return 
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
-        await client.send_message(Config.LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
 
 
 @Client.on_message(filters.command(["help"]) & filters.private)

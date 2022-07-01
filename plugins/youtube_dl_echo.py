2f974b3a333d883d8191758ed28b05f089069dee
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-07-01 23:03:57 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -37,25 +37,27 @@ from PIL import Image
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-#    if Config.LOG_CHANNEL:
-#        try:
-#            log_message = await message.forward(Config.LOG_CHANNEL)
-#            log_info = "Message Sender Information\n"
-#            log_info += "\nFirst Name: " + update.from_user.first_name
-#            log_info += "\nUser ID: " + update.from_user.id
-#            if update.from_user.username:
-#                log_info += "\nUsername: " + update.from_user.username
-#            log_info += "\nUser Link: " + update.from_user.mention
-#            await log_message.reply_text(
-#                text=log_info,
-#                disable_web_page_preview=True,
-#                quote=True
-#            )
-#        except Exception as error:
-#            print(error)
+    if Config.LOG_CHANNEL:
+        try:
+            log_message = await message.forward(Config.LOG_CHANNEL)
+            log_info = "Message Sender Information\n"
+            log_info += "\nFirst Name: " + update.from_user.first_name
+            log_info += "\nUser ID: " + update.from_user.id
+            if update.from_user.username:
+                log_info += "\nUsername: " + update.from_user.username
+            log_info += "\nUser Link: " + update.from_user.mention
+            await log_message.reply_text(
+                text=log_info,
+                disable_web_page_preview=True,
+                quote=True
+            )
+        except Exception as error:
+            print(error)
+            
     logger.info(update.from_user.id)
     fmsg = await update.reply_text(text=Translation.CHECKING_LINK, quote=True)
     url = update.text
+    
     if Config.UPDATE_CHANNEL:
         try:
             user = await bot.get_chat_member(Config.UPDATE_CHANNEL, update.from_user.id)
@@ -68,19 +70,20 @@ async def echo(bot, update):
         except Exception:
             await bot.edit_message_text(chat_id=update.chat.id, text=Translation.SOMETHING_WRONG, message_id=fmsg.message_id)
             return
-    if update.from_user.id not in Config.AUTH_USERS:
-        # restrict free users from sending more links
-        if str(update.from_user.id) in Config.ADL_BOT_RQ:
-            current_time = time.time()
-            previous_time = Config.ADL_BOT_RQ[str(update.from_user.id)]
-            process_max_timeout = round(Config.PROCESS_MAX_TIMEOUT/60)
-            present_time = round(Config.PROCESS_MAX_TIMEOUT-(current_time - previous_time))
-            Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
-            if round(current_time - previous_time) < Config.PROCESS_MAX_TIMEOUT:
-                await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=fmsg.message_id)
-                return
-        else:
-            Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
+        if update.from_user.id not in Config.AUTH_USERS:
+                # restrict free users from sending more links
+            if str(update.from_user.id) in Config.ADL_BOT_RQ:
+                current_time = time.time()
+                previous_time = Config.ADL_BOT_RQ[str(update.from_user.id)]
+                process_max_timeout = round(Config.PROCESS_MAX_TIMEOUT/60)
+                present_time = round(Config.PROCESS_MAX_TIMEOUT-(current_time - previous_time))
+                Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
+                if round(current_time - previous_time) < Config.PROCESS_MAX_TIMEOUT:
+                    await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=fmsg.message_id)
+                    return
+            else:
+                Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
+    
     youtube_dl_username = None
     youtube_dl_password = None
     file_name = None
